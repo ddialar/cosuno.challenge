c1ad77899277a88a2f67b743b648f0719805b2a1
ddialar <ddialar@gmail.com>
2022-08-10 10:01:42 +0100

    chore(repository): initialize the repository.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
