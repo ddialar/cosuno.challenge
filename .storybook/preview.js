dd96a461632757097d3b77572065f56dadc30ab5
ddialar <ddialar@gmail.com>
2022-08-12 22:56:02 +0100

    chore(storybook): configure the preview script in order to load unoptimized images.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
 import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css';
 
+import * as NextImage from 'next/image'
+
+const OriginalImage = NextImage.default
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalImage {...props} unoptimized />
+})
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
