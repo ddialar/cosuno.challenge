ee8f47e806b3e4fba0706323c4c308a66aaa3a7a
ddialar <ddialar@gmail.com>
2022-08-12 23:50:46 +0100

    test(companycard): implement the testing suite.

---
+++ src/view/pages/Home/components/CompanyCard/CompanyCard.test.tsx
@@ -0,0 +1,68 @@
+import type { ComponentProps } from 'react'
+import { render } from '@testing-library/react'
+import { CompanyCard } from './CompanyCard.component'
+import '@testing-library/jest-dom'
+
+const setup = (properties: ComponentProps<typeof CompanyCard>) =>
+  render(<CompanyCard {...properties} />)
+
+describe('CompanyCard component', () => {
+  it('render success', () => {
+    const properties: ComponentProps<typeof CompanyCard> = {
+      logoUrl: '/img/logos/consuno-avatar.webp',
+      name: 'Monsters, Inc.',
+      city: 'Monstropolis',
+      specialities: ['Energy']
+    }
+    const { getByAltText, getByLabelText, getByText } = setup({ ...properties })
+
+    const logo = getByAltText(`${properties.name} logo`)
+    expect(logo).toBeInTheDocument()
+
+    const companyName = getByLabelText('company-name')
+    expect(companyName).toBeInTheDocument()
+    expect(companyName).toHaveTextContent(properties.name)
+
+    const companyCity = getByLabelText('company-city')
+    expect(companyCity).toBeInTheDocument()
+    expect(companyCity).toHaveTextContent(properties.city)
+
+    const companySpecialities = getByLabelText('company-specialities')
+    expect(companySpecialities).toBeInTheDocument()
+    properties.specialities.forEach(speciality =>
+      expect(getByText(speciality)).toBeInTheDocument()
+    )
+  })
+
+  it('render default logo when it is not provided', () => {
+    jest.mock('next/image', () => ({ src, alt }: { src: string; alt: string }) => <img src={src} alt={alt} />)
+
+    const properties: ComponentProps<typeof CompanyCard> = {
+      logoUrl: '',
+      name: 'Monsters, Inc.',
+      city: 'Monstropolis',
+      specialities: ['Energy']
+    }
+    const expectedResult = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+    const { getByAltText } = setup({ ...properties })
+
+    const logo = getByAltText(`${properties.name} logo`)
+    expect(logo).toBeInTheDocument()
+    expect(logo).toHaveProperty('src', expectedResult)
+
+    jest.resetAllMocks()
+  })
+
+  it('specialities section is not rendered when they are not provided', () => {
+    const properties: ComponentProps<typeof CompanyCard> = {
+      logoUrl: '/img/logos/consuno-avatar.webp',
+      name: 'Monsters, Inc.',
+      city: 'Monstropolis',
+      specialities: []
+    }
+    const { queryByLabelText } = setup({ ...properties })
+
+    const companySpecialities = queryByLabelText('company-specialities')
+    expect(companySpecialities).not.toBeInTheDocument()
+  })
+})
