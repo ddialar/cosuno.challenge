180301894e6807a3fad9544a8e9d68ffe255a8f4
ddialar <ddialar@gmail.com>
2022-08-12 19:49:18 +0100

    chore(companycard): implement the basic structure for the card.

--- src/view/pages/Home/Home.page.tsx
+++ src/view/pages/Home/Home.page.tsx
@@ -1,7 +1,8 @@
+import { useState } from 'react'
+import Image from 'next/image'
 import { MainLayout } from '@layouts'
 import { Header } from '@components'
 import { SearchBox } from './components'
-import { useState } from 'react'
 
 const availableFilters = [
   'Excavation',
@@ -33,6 +34,25 @@ export const HomePage = () => {
           searchParams={searchParams}
           onSearchChange={onSearchChange}
         />
+        <ul>
+          <li className='px-4 py-2'>
+            <div role='company-card' className='flex md:flex-col bg-white drop-shadow-lg rounded-md' style={{ maxHeight: '100px' }}>
+              <div>
+                <Image src={'/img/logos/bechtel-logo.png'} width='100%' height='100%' className='rounded-tl-md rounded-bl-md' />
+              </div>
+              <div className='flex flex-col pt-1 pb-2 pl-2'>
+                <span>Company name</span>
+                <span className='text-sm'>City</span>
+                <ul className='flex flex-1 items-end text-sm text-blue-500'>
+                  <li className='px-2 mr-1 border border-blue-500 rounded-md'>Esp. #1</li>
+                  <li className='px-2 mr-1 border border-blue-500 rounded-md'>Esp. #2</li>
+                  <li className='px-2 mr-1 border border-blue-500 rounded-md'>Esp. #3</li>
+                </ul>
+              </div>
+            </div>
+            {/* Insert a block with the company information */}
+          </li>
+        </ul>
       </main>
       <footer className='flex'>
         Challenge done by...
