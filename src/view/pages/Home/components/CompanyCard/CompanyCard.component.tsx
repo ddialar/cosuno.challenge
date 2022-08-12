e5e737b92ae304358933ed65db9e3c667b083e4b
ddialar <ddialar@gmail.com>
2022-08-12 23:50:13 +0100

    refactor(companycard): edit the component in order to now show the specialities section when they are not provided.

--- src/view/pages/Home/components/CompanyCard/CompanyCard.component.tsx
+++ src/view/pages/Home/components/CompanyCard/CompanyCard.component.tsx
@@ -15,21 +15,26 @@ export const CompanyCard: FC<Props> = ({ logoUrl, name, city, specialities }) =>
       width='100%'
       height='100%'
       className='rounded-tl-md rounded-bl-md'
+      alt={`${name} logo`}
     />
     <div className='flex flex-col flex-1 px-3 pt-1 pb-2'>
-      <div>{name}</div>
-      <div className='text-sm'>{city}</div>
-      <ul className='flex flex-1 items-end text-sm text-blue-main'>
-        {
-          specialities.map(speciality =>
-            <li
-              key={`${name}-${speciality}`}
-              className='px-2 mr-1 border border-blue-main rounded-md'
-            >
-              {speciality}
-            </li>
-          )
-        }
-      </ul>
+      <div aria-label='company-name'>{name}</div>
+      <div aria-label='company-city' className='text-sm'>{city}</div>
+      {
+        specialities.length
+          ? <ul aria-label='company-specialities' className='flex flex-1 items-end text-sm text-blue-main'>
+            {
+              specialities.map(speciality =>
+                <li
+                  key={`${name}-${speciality}`}
+                  className='px-2 mr-1 border border-blue-main rounded-md'
+                >
+                  {speciality}
+                </li>
+              )
+            }
+          </ul>
+          : null
+      }
     </div>
   </div>
