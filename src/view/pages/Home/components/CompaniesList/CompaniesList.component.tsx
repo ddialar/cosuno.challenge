46a25c88ff5405c918db8fb1a288308c5c295a19
ddialar <ddialar@gmail.com>
2022-08-13 17:44:53 +0100

    styles(companieslist): adapt the component in order to fit to desktop screens.

--- src/view/pages/Home/components/CompaniesList/CompaniesList.component.tsx
+++ src/view/pages/Home/components/CompaniesList/CompaniesList.component.tsx
@@ -8,10 +8,10 @@ interface Props {
 export const CompaniesList: FC<Props> = ({ companies }) =>
   <div className='flex-1 relative'>
     <div className='absolute w-full h-full overflow-y-scroll'>
-      <ul className='block md:flex md:flex-wrap md:justify-between md:px-2 md:py-3'>
+      <ul className='block md:flex md:flex-wrap md:justify-between py-2 md:px-2 md:py-3'>
         {
-          companies.map(company =>
-            <li key={company.name} className='px-4 py-2 md:px-2'>
+          companies.map((company, index) =>
+            <li key={`${company.name} ${index}`} className='px-4 py-2 md:px-2'>
               <CompanyCard {...company} />
             </li>
           )
