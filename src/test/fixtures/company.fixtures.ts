98f28bb7b2312b080d4aed7df3850561ed8d9427
ddialar <ddialar@gmail.com>
2022-08-14 17:33:38 +0100

    test(fixtures): implement the company fixtures.

---
+++ src/test/fixtures/company.fixtures.ts
@@ -0,0 +1,107 @@
+export const companyFixtures: CompanyData[] = [
+  {
+    logoUrl: '/img/logos/monsters-inc-logo.webp',
+    name: 'Monsters, Inc.',
+    city: 'Monstropolis',
+    specialities: ['Electrical']
+  },
+  {
+    logoUrl: '/img/logos/acme-corp-logo.webp',
+    name: 'ACME, Corp.',
+    city: 'Arizona',
+    specialities: ['Demolition']
+  },
+  {
+    logoUrl: '/img/logos/krayton-tags-logo.webp',
+    name: 'Krayton Tags',
+    city: 'Berlin',
+    specialities: [
+      'Excavation',
+      'Plumbing',
+      'Bricklaying',
+      'Stonemasonry',
+      'Demolition',
+      'Painting',
+      'Decorating'
+    ]
+  },
+  {
+    logoUrl: '/img/logos/property-brothers-logo.webp',
+    name: 'Property Brothers',
+    city: 'New York',
+    specialities: [
+      'Plumbing',
+      'Electrical',
+      'Bricklaying',
+      'Stonemasonry',
+      'Carpentry',
+      'Fencer',
+      'Painting',
+      'Decorating'
+    ]
+  },
+  {
+    logoUrl: '/img/logos/smith-engineering-co-logo.webp',
+    name: 'Smiths Engineering, Co.',
+    city: 'Boston',
+    specialities: [
+      'Excavation',
+      'Electrical',
+      'Bricklaying',
+      'Stonemasonry',
+      'Demolition'
+    ]
+  },
+  {
+    logoUrl: '/img/logos/stark-industries-logo.webp',
+    name: 'Stark Industries',
+    city: 'New York',
+    specialities: [
+      'Electrical',
+      'Demolition',
+      'Scaffolding'
+    ]
+  },
+  {
+    logoUrl: '/img/logos/umbrela-corporation-logo.webp',
+    name: 'Umbrela Corp.',
+    city: 'Tokio',
+    specialities: [
+      'Plumbing',
+      'Electrical',
+      'Bricklaying',
+      'Stonemasonry',
+      'Carpentry',
+      'Fencer'
+    ]
+  },
+  {
+    logoUrl: '/img/logos/wayne-enterprises-logo.webp',
+    name: 'Wayne Enterprises',
+    city: 'Gotham',
+    specialities: [
+      'Excavation',
+      'Plumbing',
+      'Electrical',
+      'Bricklaying',
+      'Stonemasonry',
+      'Carpentry',
+      'Fencer',
+      'Demolition',
+      'Scaffolding'
+    ]
+  },
+  {
+    logoUrl: '/img/logos/weyland-yutani-corp-logo.webp',
+    name: 'Weyland-Yutani, Corp.',
+    city: 'San Francisco',
+    specialities: [
+      'Excavation',
+      'Plumbing',
+      'Electrical',
+      'Stonemasonry',
+      'Fencer',
+      'Demolition'
+    ]
+  }
+]
