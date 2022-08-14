1c13640d35dad6600329fe916d641fcf1113f0af
ddialar <ddialar@gmail.com>
2022-08-14 16:20:00 +0100

    styles(companycard): wrap comany logo into a container, set the same height to the whole cards in descktop views and reduce the specialities list font size.

--- src/view/pages/Home/components/CompanyCard/CompanyCard.component.tsx
+++ src/view/pages/Home/components/CompanyCard/CompanyCard.component.tsx
@@ -4,25 +4,27 @@ import Image from 'next/image'
 interface Props extends CompanyData {}
 
 export const CompanyCard: FC<Props> = ({ logoUrl, name, city, specialities }) =>
-  <div role='company-card' className='flex bg-white drop-shadow-lg rounded-md md:w-[397px]'>
-    <Image
-      src={logoUrl || '/img/logos/consuno-avatar.webp'}
-      width='100%'
-      height='100%'
-      className='rounded-tl-md rounded-bl-md'
-      alt={`${name} logo`}
-    />
+  <div role='company-card' className='flex h-full bg-white drop-shadow-lg rounded-md md:w-[397px]'>
+    <div className='px-1'>
+      <Image
+        src={logoUrl || '/img/logos/consuno-avatar.webp'}
+        width='100%'
+        height='100%'
+        className='rounded-tl-md rounded-bl-md'
+        alt={`${name} logo`}
+      />
+    </div>
     <div className='flex flex-col flex-1 px-3 pt-1 pb-2'>
       <div aria-label='company-name' className='font-bold'>{name}</div>
       <div aria-label='company-city' className='text-sm'>{city}</div>
       {
         specialities.length
-          ? <ul aria-label='company-specialities' className='flex flex-1 items-end text-sm text-blue-main'>
+          ? <ul aria-label='company-specialities' className='flex flex-wrap w-full mt-2 items-end text-xs text-blue-main'>
             {
               specialities.map(speciality =>
                 <li
                   key={`${name}-${speciality}`}
-                  className='px-2 mr-1 border border-blue-main rounded-md'
+                  className='px-2 mt-1 mr-1 border border-blue-main rounded-md'
                 >
                   {speciality}
                 </li>
