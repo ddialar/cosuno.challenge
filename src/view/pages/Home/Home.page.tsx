ff189f8f2e812028e5dcbe45cc4395973151f94c
ddialar <ddialar@gmail.com>
2022-08-15 19:11:30 +0100

    chore(homepage): apply the 'isLoading' property.

--- src/view/pages/Home/Home.page.tsx
+++ src/view/pages/Home/Home.page.tsx
@@ -1,5 +1,5 @@
 import { MainLayout } from '@layouts'
-import { Header, Footer, EmojiSadIcon } from '@components'
+import { Header, Footer, EmojiSadIcon, Loading } from '@components'
 import { useCompany } from '@hooks'
 import { SearchBox, CompaniesList } from './components'
 
@@ -19,7 +19,7 @@ const availableFilters = [
 ]
 
 export const HomePage = () => {
-  const { companies, searchParams, setSearchParams } = useCompany()
+  const { companies, searchParams, setSearchParams, isLoading } = useCompany()
   const onSearchChange = (args: SearchParams) => {
     setSearchParams({ ...args })
   }
@@ -33,24 +33,15 @@ export const HomePage = () => {
           searchParams={searchParams}
           onSearchChange={onSearchChange}
         />
-        {/* {
-          companies
-            ? companies && companies.length
+        {
+          isLoading
+            ? <Loading text='loading companies' />
+            : companies && companies.length
               ? <CompaniesList companies={companies} />
               : <div className={'flex flex-col flex-1 justify-center items-center text-gray-500 text-xl'}>
                 <EmojiSadIcon className='w-10 h-10' />
                 No companies found
               </div>
-              // : <span>Loading...</span>
-            : <span>Loading...</span>
-        } */}
-        {
-          companies && companies.length
-            ? <CompaniesList companies={companies} />
-            : <div className={'flex flex-col flex-1 justify-center items-center text-gray-500 text-xl'}>
-              <EmojiSadIcon className='w-10 h-10' />
-                  No companies found
-            </div>
         }
       </main>
       <Footer />
